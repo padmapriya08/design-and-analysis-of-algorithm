7361027d838695f7e51ac4f05f18f86404c81bdf
padmapriya08 <113330303+padmapriya08@users.noreply.github.com>
2023-08-16 10:29:00 +0530

    Add files via upload

---
+++ largest element DAA.c
@@ -0,0 +1,31 @@
+
+
+#include <stdio.h>
+
+int main() {
+    int n, i;
+    
+    printf("Enter the size of the array: ");
+    scanf("%d", &n);
+    
+    int arr[n];
+    
+    printf("Enter the elements of the array: ");
+    for(i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+    
+    int max = arr[0];
+    
+    for(i = 1; i < n; i++) {
+        if(arr[i] > max) {
+            max = arr[i];
+        }
+    }
+    
+    printf("The largest element in the array is: %d\n", max);
+    
+    return 0;
+}
+
+
